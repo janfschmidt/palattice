dbdef910f747eab262aae56d979e7b3962869acc
Jan Schmidt <schmidt@physik.uni-bonn.de>
2012-02-02 13:36:12 +0100

    Bsupply/main.cpp: printf->cout

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 /* Calculation of the magnetic spectrum (horizontal, vertical) of a periodic accelerator  *
  * Based on MadX output data and (optional for ELSA) on measured orbit and corrector data *
  * Used as input for Simulations of polarization by solving Thomas-BMT equation           *
- * 27.01.2012 - J.Schmidt                                                                 *
+ * 02.02.2012 - J.Schmidt                                                                 *
  */
 
 #include <stdio.h>
@@ -31,7 +31,7 @@ int main (int argc, char *argv[])
   double t = 0.531;    // moment of elsa-cycle (s)
   char importFile[1024] = "../project/madx/elsa_test.orbit";
   char spurenFolder[1024] = "../project/ELSA-Spuren/2012-01-24-15-11-27";
-  bool elsa = true;       // true: orbit, correctors, k & m read from /sgt/elsa/bpm/... 
+  bool elsa = false;       // true: orbit, correctors, k & m read from /sgt/elsa/bpm/... 
 
   char filename[1024];
   string tmp;
@@ -51,11 +51,11 @@ int main (int argc, char *argv[])
 
   // check input
   if (fmax_x >= n_samp/2 || fmax_z >= n_samp/2) {
-    printf("\nThe maximum frequency is to large to be calculated with %d sampling points.\n", n_samp);
+    cout << "ERROR: The maximum frequency is to large to be calculated with "<<n_samp<<" sampling points." << endl;
     return 1;
   }
   if (t < 0.0) {
-    printf("t = %lf < 0 is no valid moment of ELSA cycle.\n", t);
+    cout << "ERROR: t = "<<t<<" < 0 is no valid moment of ELSA cycle." << endl;
     return 1;
   }
 
@@ -87,7 +87,7 @@ int main (int argc, char *argv[])
       }
     }
     else {
-      cout << "ERROR: first argument sets mode: -elsa or -madx" << endl;
+      cout << "First argument sets mode: -elsa or -madx" << endl;
       return 1;
     }
   }
@@ -101,7 +101,7 @@ int main (int argc, char *argv[])
   metadata.add("personal message", "juchu, es klappt :-)");
   tmp = metadata.getbyLabel("LENGTH");
   if (tmp == "NA") {
-    printf("cannot read accelerator circumference from MadX file.\n");
+    cout << "ERROR: cannot read accelerator circumference from MadX file." << endl;
     return 1;
   }
   else {
@@ -110,11 +110,12 @@ int main (int argc, char *argv[])
 
   
   // output
-  printf("\n---------------------------------\n");
-  printf("calculate magnetic field spectrum\n");
-  printf("---------------------------------\n");
-  printf("* %d sampling points along ELSA ring\n", n_samp);
-  printf("* maximum frequencies used for B-field evaluation: Bx:%d, Bz:%d\n", fmax_x, fmax_z);
+  cout << endl;
+  cout << "--------------------------------------------" << endl;
+  cout << "Bsupply: calculate magnetic field & spectrum" << endl;
+  cout << "--------------------------------------------" << endl;
+  cout << "* "<<n_samp<<" sampling points along ELSA ring" << endl;
+  cout << "* maximum frequencies used for B-field evaluation: Bx:"<<fmax_x<<", Bz:"<<fmax_z << endl;
 
 
   // read particle orbit and lattice (magnet positions & strengths) from MADX
@@ -126,12 +127,12 @@ int main (int argc, char *argv[])
     ELSAimport(ELSAbpms, ELSAvcorrs, quads, sexts, spurenFolder); 
     ELSAimport_getbpmorbit(ELSAbpms, bpmorbit, t);
     ELSAimport_getvcorrs(ELSAvcorrs, vcorrs, t);
-    printf("* %d dipoles, %d quadrupoles, %d sextupoles read from %s.\n", dipols.size(), quads.size(), sexts.size(), importFile);
-    printf("* %d correctors and %i BPMs read from %s.\n", vcorrs.size(), bpmorbit.size(), spurenFolder);
+    cout << "* "<<dipols.size()<<" dipoles, "<<quads.size()<<" quadrupoles, "<<sexts.size()<<" sextupoles read from "<<importFile << endl;
+    cout << "* "<<vcorrs.size()<<" correctors and "<<bpmorbit.size()<<" BPMs read from "<<spurenFolder << endl;
   }
   else {
     metadata.add("Program Mode", "madx");
-    printf("* %d dipoles, %d quadrupoles, %d sextupoles, %d correctors and %d BPMs read from %s.\n", dipols.size(), quads.size(), sexts.size(), vcorrs.size(), bpmorbit.size(), importFile);
+    cout << "* "<<dipols.size()<<" dipoles, "<<quads.size()<<" quadrupoles, "<<sexts.size()<<" sextupoles, "<<vcorrs.size()<<" correctors and "<<bpmorbit.size()<<" BPMs read from "<<importFile << endl;
   }
 
   // interpolate orbit, calculate field distribution & spectrum
@@ -141,7 +142,7 @@ int main (int argc, char *argv[])
 
 
   // generate output files
-  printf("Write output files:\n");
+  cout << "Write output files:" << endl;
 
   if (elsa) {
     //BPM data
@@ -161,12 +162,12 @@ int main (int argc, char *argv[])
   snprintf(filename, 1024, "%s/eval.dat", OUTPUTFOLDER);
   eval_out(bx, bz, fmax_x, fmax_z, n_samp, circumference, filename);
   //export spectrum files for polarization-calculation
-  exportfile(bx, fmax_x, metadata, "horizontal", true);
-  exportfile(bx, fmax_x, metadata, "horizontal", false); //second file without timestamp for gnuplot-script
-  exportfile(bz, fmax_z, metadata, "vertical", true);
-  exportfile(bz, fmax_z, metadata, "vertical", false);   //second file without timestamp for gnuplot-script
+  //exportfile(bx, fmax_x, metadata, "horizontal", true);
+  exportfile(bx, fmax_x, metadata, "horizontal", false); // file without timestamp (false)
+  //exportfile(bz, fmax_z, metadata, "vertical", true);
+  exportfile(bz, fmax_z, metadata, "vertical", false);   // file without timestamp (false)
   
-  printf("Finished. (Run polsim.gp for plots)\n\n");
+  cout << "Finished. (Run inout/Bsupply.gp for plots)" << endl << endl;
   
 
   return 0;
