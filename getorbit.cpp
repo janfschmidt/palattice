74d86f15a7f7c2585b9cb0ee8839d1ed411b08a4
Jan Schmidt <schmidt@physik.uni-bonn.de>
2012-02-07 14:47:07 +0100

    Bsupply/getorbit.cpp: extrapolation fixed (Frank)

--- getorbit.cpp
+++ getorbit.cpp
@@ -22,39 +22,61 @@ int getorbit (orbitvec &orbit, double circumference, orbitvec &bpmorbit, int n_s
   int n_bpm = bpmorbit.size();
   double interval_samp = circumference/n_samp;              /* sampling interval in meter */
   const gsl_interp_type *type = gsl_interp_akima_periodic;  /* type of interpolation used (see GSL manual) */
+  gsl_error_handler_t *old_error_handler;
 
   gsl_interp_accel *acc_x = gsl_interp_accel_alloc ();
   gsl_interp_accel *acc_z = gsl_interp_accel_alloc ();
-  gsl_spline *spline_x = gsl_spline_alloc (type, n_bpm+1);
-  gsl_spline *spline_z = gsl_spline_alloc (type, n_bpm+1);
+  gsl_spline *spline_x = gsl_spline_alloc (type, n_bpm+2);
+  gsl_spline *spline_z = gsl_spline_alloc (type, n_bpm+2);
 
-  double tmp_pos[n_bpm+1], tmp_x[n_bpm+1], tmp_z[n_bpm+1];
+  double tmp_pos[n_bpm+2], tmp_x[n_bpm+2], tmp_z[n_bpm+2];
 
   struct ORBIT otmp;
 
+  // add initial entry to avoid extrapolation (Frank)
+  tmp_pos[0] = bpmorbit[n_bpm-1].pos-circumference;
+  tmp_x[0] = bpmorbit[n_bpm-1].x;
+  tmp_z[0] = bpmorbit[n_bpm-1].z;
   for (i=0; i<n_bpm; i++) {
-    tmp_pos[i] = bpmorbit[i].pos;
-    tmp_x[i] = bpmorbit[i].x;
-    tmp_z[i] = bpmorbit[i].z;
+    tmp_pos[i+1] = bpmorbit[i].pos;
+    tmp_x[i+1] = bpmorbit[i].x;
+    tmp_z[i+1] = bpmorbit[i].z;
   }
-  /* copy first orbit entry for interpolation with periodic boundary condition */
-  tmp_pos[n_bpm] = tmp_pos[0]+circumference; 
-  tmp_x[n_bpm] = tmp_x[0];
-  tmp_z[n_bpm] = tmp_z[0];
+  // copy first orbit entry for interpolation with periodic boundary condition
+  tmp_pos[n_bpm+1] = bpmorbit[0].pos+circumference; 
+  tmp_x[n_bpm+1] = bpmorbit[0].x;
+  tmp_z[n_bpm+1] = bpmorbit[0].z;
   
-  gsl_spline_init (spline_x, tmp_pos, tmp_x, n_bpm+1); /* horizontal (x) */
-  gsl_spline_init (spline_z, tmp_pos, tmp_z, n_bpm+1); /* vertical (z) */
+  old_error_handler = gsl_set_error_handler_off();
+  gsl_spline_init (spline_x, tmp_pos, tmp_x, n_bpm+2); // horizontal (x)
+  gsl_spline_init (spline_z, tmp_pos, tmp_z, n_bpm+2); // vertical (z)
+
 
   for (i=0; i<n_samp; i++) {
     otmp.pos = i*interval_samp;
     otmp.x = gsl_spline_eval (spline_x, otmp.pos, acc_x);
+    if (isnan(otmp.x))
+      {
+	cout << "ERROR: getorbit(): interpolation error in x orbit, otmp.pos="<<otmp.pos<< endl;
+	otmp.x = 0.;
+      }
     otmp.z = gsl_spline_eval (spline_z, otmp.pos, acc_z);
+    if (isnan(otmp.z))
+      {
+	cout << "ERROR: getorbit(): interpolation error in z orbit, otmp.pos="<<otmp.pos<< endl;
+	otmp.z = 0.;
+      }
+    #if DEBUG
+      cout << "pos="<<otmp.pos<<"  x="<<otmp.x<<"  z="<<otmp.z << endl;
+    #endif
     orbit.push_back(otmp);
   }
+
   gsl_spline_free (spline_x);
   gsl_interp_accel_free (acc_x);
   gsl_spline_free (spline_z);
   gsl_interp_accel_free (acc_z);
+  gsl_set_error_handler(old_error_handler);
 
   return 0;
 }
