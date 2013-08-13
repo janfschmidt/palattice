549e4e4c2fa12346e22314dd565adfe2905ff780
Jan Schmidt <schmidt@physik.uni-bonn.de>
2013-08-13 15:52:58 +0200

    Approach failed: force equal number of samples per turn gets to complicated when implementing set() or push_back().

--- functionofpos.cpp
+++ functionofpos.cpp
@@ -6,6 +6,7 @@
 
 #include "functionofpos.hpp"
 
+using namespace std;
 
 
 // constructor
@@ -19,14 +20,13 @@ FunctionOfPos::FunctionOfPos(gsl_interp_type *t, double periodIn, double circIn)
 //get index for pos[] & value[] from index(1turn) and turn
 unsigned int FunctionOfPos::index(unsigned int i, unsigned int turn) const
 {
-  if (turn == 1)
-    return i;
-  else if (turn > 1)
-    return samples()*(turn-1) + i - 1;   //("- 1" => starting at index 0)
-    else  {
-      cout << "ERROR: FunctionOfPos:index(): turn=0 is not defined. Set turn=1 (first turn)." << endl;
-      return i;
-    }
+  if (i >= size() || turn > turns())
+    throw out_of_range();
+
+  if (turn > 1 && i >= samples())
+    throw out_of_range();
+  
+  return samples()*(turn-1) + i - 1;   //("- 1" => starting at index 0)
 }
 
 
@@ -34,83 +34,102 @@ unsigned int FunctionOfPos::index(unsigned int i, unsigned int turn) const
 //get index for pos[] & value[] from pos(1turn) and turn
 unsigned int FunctionOfPos::index(double pos, unsigned int turn) const
 {
-  unsigned int i;
-  double tmpPos = getPos(pos,turn);
+  unsigned int j;
+  double tmpPos = posTotal(pos,turn);
 
-  for (i=0; i<size(); i++) {
-    if (pos[i] == tmpPos)
-      return i;
-    else if (pos[i] > tmpPos)
-      break;
+  for (j=0; j<size(); j++) {
+    if (pos[j] == tmpPos)
+      return j;
+    else if (pos[j] > tmpPos)
+      throw eNoData(j);
   }
-  cout << "WARNING: FunctionOfPos:index(): There is no Sample Point at pos=" << pos
-       << "& turn=" << turn <<". Return next sample point at pos=" << pos[i] << endl;
-  return i;
+
+  throw eNoData(j);
 }
 
 
 
 // Turn
-unsigned int FunctionOfPos::getTurn(unsigned int i) const
+unsigned int FunctionOfPos::turn(unsigned int i) const
 {
-  return (pos[i]%circ) + 1;
+  return (i%samples()) + 1;
 }
 
-unsigned int FunctionOfPos::getTurn(double posIn) const
+unsigned int FunctionOfPos::turn(double posIn) const
 {
   return (posIn%circ) + 1;
 }
 
 
 
-// Sample (e.g. "which BPM?")
-unsigned int FunctionOfPos::getSample(unsigned int i) const
+// calculate Sample (e.g. "which BPM?")
+unsigned int FunctionOfPos::sample(unsigned int i) const
 {
   return i - (samples() * (getTurn(i)-1));
 }
 
+
+
+// calculate pos within turn
+double FunctionOfPos::posInTurn(double pos) const
+{
+  return pos - ((pos%circ) * circ);
+}
+
+
+
+// calculate "absolute pos" from "pos within turn" and turn
+double FunctionOfPos::posTotal(double posInTurn, unsigned int turn) const
+{
+  return  posInTurn + circ*(turn-1);
+}
+
+
+
+// get sample ("index modulo turn") by pos, IF IT EXISTS
 unsigned int FunctionOfPos::getSample(double posIn) const
 {
   unsigned int i;
-  for (i=0; i<size(); i++) {
-    if (pos[i] == posIn)
-      return getSample(i);
-    else if (pos[i] > posIn) {
-      break;
-    }
+
+  try {
+    i = index(posIn);
   }
-  cout << "WARNING: FunctionOfPos:getSample(): There is no Sample Point at pos=" << posIn
-       << ". Return next sample point at pos=" << pos[i-1] << endl;
+  catch (eNoData &e) {
+    cout << "WARNING: FunctionOfPos:getSample(): There is no Sample Point at pos=" << posIn << endl;
+    exit(1);
+  }
+
   return getSample(i);
 }
 
 
 
-// Position
+// get position
 double FunctionOfPos::getPos(unsigned int i, unsigned int turn) const
 {
-  return pos[index(i,turn)];
+  try {
+    return pos[index(i,turn)];
+  }
+  catch (out_of_range) {
+    cout << "ERROR: FunctionOfPos:getPos(): Index "<<i<<" turn "<<turn<<" is out of range ("
+	 <<samples()<<" samples, " <<turns()<< " turns)" << endl;
+    exit(1);
+  }
 }
 
-double FunctionOfPos::getPos(double pos1turn, unsigned int turn) const
-{
-  double tmpPos;
-  
-  if (turn == 1) tmpPos = pos1turn;
-  else if (turn > 1) tmpPos = circ*(turn-1) + pos1turn;
-  else {
-    cout << "ERROR: FunctionOfPos:getPos(): turn=0 is not defined. Set turn=1 (first turn)." << endl;
-    tmpPos = pos1turn;
-  }
-  
-  return tmpPos;
-}
 
 
 // get Value
 double FunctionOfPos::get(unsigned int i, unsigned int turn) const
 {
-  return value[index(i,turn)];
+  try {
+    return value[index(i,turn)];
+  }
+  catch (out_of_range) {
+    cout << "ERROR: FunctionOfPos:getPos(): Index "<<i<<" turn "<<turn<<" is out of range ("
+	 <<samples()<<" samples, " <<turns()<< " turns)" << endl;
+    exit(1);
+  }
 }
 
 double FunctionOfPos::get(double pos, unsigned int turn) const
@@ -122,17 +141,42 @@ double FunctionOfPos::get(double pos, unsigned int turn) const
 
 
 
-// set Value
+// modify value
 void set(double valueIn, unsigned int i, unsigned int turn)
 {
-  // wenn vorhanden, value[index(i,turn)] = valueIn
-  // sonst insert
+  try {
+    unsigned int tmpIndex = index(i,turn);
+  }
+  catch (out_of_range) {
+    cout << "ERROR: FunctionOfPos:set(): Element "<<tmpIndex
+	 << " is out of range. Please use push_back(value,position) to append new data." << endl;
+    cout << "Data is not changed. Continue." << endl;
+    return;
+  }
+
+  value[tmpIndex] = valueIn;
+}
+
+void set(double valueIn, double posIn, unsigned int turn)
+{
+  try {
+    unsigned int tmpIndex = index(posIn,turn);
+  }
+  catch (eNoData) {
+    cout << "ERROR: FunctionOfPos:set(): There is no data for pos="
+	 <<posIn<< " and turn=" <<turn<< endl;
+    cout << "Data is not changed. Continue." << endl;
+    return;
+  }
+
+  value[tmpIndex] = valueIn; 
 }
 
 
 
-void set(double value, double pos, unsigned int turn)
+void FunctionOfPos::push_back(double valueIn, double posIn, unsigned int turn)
 {
-    // wenn vorhanden, value[index(pos,turn)] = valueIn
-  // sonst insert
+
+  pos.push_back(posIn);
+  value.push_back(valueIn);
 }
--- functionofpos.hpp
+++ functionofpos.hpp
@@ -6,6 +6,8 @@
 #ifndef __FUNCTIONOFPOS_HPP_
 #define __FUNCTIONOFPOS_HPP_
 
+#inlcude <exception>
+#include <stdexcept>
 #include "interpolate.hpp"
 
 
@@ -13,10 +15,11 @@
 class FunctionOfPos : public Interpolate {
 
 protected:
+  // "data" (stored in Interpolate members)
   vector<double> &pos;                        //position: absolute, increasing with turn
   vector<double> &value;
 
-  double circ;                                //circumference of accelerator
+  const double circ;                          //circumference of accelerator
   unsigned int n_turns;                       //number of turns
   unsigned int n_samples;                     //number of samples per turn
 
@@ -24,7 +27,7 @@ protected:
 private:
   FunctionOfPos(vector<double> *posIn); //...
   unsigned int index(unsigned int i, unsigned int turn) const; //get index for pos[] & value[] from index(1turn) and turn
-  unsigned int index(double pos, unsigned int turn) const; //get index for pos[] & value[] from pos(1turn) and turn
+  unsigned int index(double pos, unsigned int turn) const; //get index for pos[] & value[] from pos(1turn) and turn. returns -1 if there is no data for given pos
 
 
 public:
@@ -33,23 +36,39 @@ public:
 
   unsigned int turns() const {return n_turns;}
   unsigned int samples() const {return n_samples;}
-  unsigned int size() const {return value->size();}
+  unsigned int size() const {return value.size();}
+  double posMax() const {return pos.back();}
 
-  unsigned int getTurn(unsigned int i) const;
-  unsigned int getTurn(double pos) const;
-  unsigned int getSample(unsigned int i) const;
-  unsigned int getSample(double pos) const;
+  unsigned int turn(unsigned int i) const;
+  unsigned int turn(double pos) const;
+  unsigned int sample(unsigned int i) const;
+  double posInTurn(double pos) const;
+  double posTotal(double posInTurn, unsigned int turn) const;
 
-  double getPos(unsigned int i, unsigned int turn=1) const;//get pos by index or by index(1turn) and turn
-  double getPos(double pos1turn, unsigned int turn=1) const; //get pos by pos1turn and turn
-  double get(unsigned int i, unsigned int turn=1) const;   //get value by index or by index(1turn) and turn
-  double get(double pos, unsigned int turn=1) const;       //get value by pos or by pos(1turn) and turn
+  //-------- these functions depend on data. thus they can fail (program exit) -----------------------
+  unsigned int getSample(double pos) const;                  //get sample ("index modulo turn") by pos, IF IT EXISTS
+  double getPos(unsigned int i, unsigned int turn=1) const;  //get pos-DATA by index or by index(1turn) and turn
+  double get(unsigned int i, unsigned int turn=1) const;     //get value-DATA by index or by index(1turn) and turn
+  double get(double pos, unsigned int turn=1) const;         //get value-DATA by pos or by pos(1turn) and turn
 
-  void set(double valueIn, unsigned int i, unsigned int turn=1);//set value by index or by index(1turn) and turn
-  void set(double valueIn, double pos, unsigned int turn=1);    //set value by pos or by pos(1turn) and turn
+  // these functions modify data
+  void set(double valueIn, unsigned int i, unsigned int turn=1);  //modify value by index or by index(1turn) and turn
+  void set(double valueIn, double pos, unsigned int turn=1);      //modify value by pos or by pos(1turn) and turn
+  void push_back(double valueIn, double pos, unsigned int turn=1);//add value&pos or value&pos(1turn)&turn
   void clear();
 
-  void out(char *filename) const;
+  void out(char *filename) const;  //output to file
+};
+
+
+
+
+// exceptions
+class eNoData : public std::exception {
+public:
+  const unsigned int index;
+
+  eNoData(unsigned int indexIn) : index(indexIn) {};
 };
 
 
