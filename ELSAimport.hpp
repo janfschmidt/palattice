d6dce6e1f53970c68c01e2864e77296213be332d
Jan Schmidt <schmidt@physik.uni-bonn.de>
2014-01-16 17:46:20 +0100

    ELSAimport: deleted unused functions

--- ELSAimport.hpp
+++ ELSAimport.hpp
@@ -7,8 +7,6 @@ int ELSAimport_bpms(BPM *ELSAbpms, const char *spurenFolder);
 
 int ELSAimport_vcorrs(CORR *ELSAvcorrs, const char *spurenFolder);
 
-//int corrs_out(magnetvec vcorrs, const char *filename);
-
 #endif
 
 /*__POLSIM__ELSAIMPORT_HPP_*/
